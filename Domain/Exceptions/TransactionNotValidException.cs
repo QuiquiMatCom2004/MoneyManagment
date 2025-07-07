2742073874a23e7058add62841e7055f4363fe53
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-06 22:26:26 -0400

    feat: Implemented Transaction Exception

---
+++ Domain/Exceptions/TransactionNotValidException.cs
@@ -0,0 +1,27 @@
+﻿using Domain.ValueObjects; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class TransactionNotValidException : Exception
+    {
+        public DateTime _date = DateTime.UtcNow;
+        public Money Money { get; set; } = new Money(0, "USD");
+        public TransactionNotValidException() : base("The transaction is not Valid")
+        {
+
+        }
+        public TransactionNotValidException(DateTime date) : base($"The transaction date {date} is not valid")
+        {
+            _date = date;
+        }
+        public TransactionNotValidException(Money money) : base($"the mount {money.Amount} its not valid")
+        {
+            Money = money;
+        }
+    }
+}
