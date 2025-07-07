763baa15e3d331b4a84b088ec32c48acf8a63ddf
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-07 01:21:06 -0400

    feat: add CurrencyNotValidOperationException class
    Se ha añadido una nueva clase `CurrencyNotValidOperationException` en el espacio de nombres `Domain.Exceptions` para manejar excepciones relacionadas con operaciones de moneda no válidas. La clase incluye propiedades para almacenar dos monedas y varios constructores para instanciar la excepción con mensajes específicos.

---
+++ Domain/Exceptions/CurrencyNotValidOperationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions;
+public class CurrencyNotValidOperationException : Exception
+{
+    public string Currency1 { get; } = string.Empty;
+    public string Currency2 { get; } = string.Empty;
+    public CurrencyNotValidOperationException() : base("The currency is not valid for this operation.")
+    {
+    }
+    public CurrencyNotValidOperationException(string message) : base(message)
+    {
+
+    }
+    public CurrencyNotValidOperationException(string currency1, string currency2) : base($"The currencies '{currency1}' and '{currency2}' are not compatible for this operation.")
+    {
+        Currency1 = currency1;
+        Currency2 = currency2;
+    }
+}
