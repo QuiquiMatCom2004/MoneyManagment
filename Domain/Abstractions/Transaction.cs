43bccb0c0f0ea8f27a3a93ddb9ead3224f5a0ce6
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-06 22:25:07 -0400

    feat: Implemented transaction abtraction

---
+++ Domain/Abstractions/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public abstract class Transaction : Entity, IValidable, IProcessable
+    {
+        public DateTime TransactionDate { get; protected set; } = DateTime.UtcNow;
+        public Money Amount { get; set; } = new Money(0, "USD");
+        public Guid RelatedEntityId { get; set; } = Guid.Empty;
+        public string Description { get; set; } = string.Empty;
+        public TransactionType Type { get; set; } = TransactionType.None;
+
+        public abstract bool Process();
+
+        public virtual bool Validate()
+        {
+            if(Amount.Amount < 0)
+                return false;
+            if(TransactionDate > DateTime.UtcNow)
+                return false;
+            return true;
+        }
+    }
+}
