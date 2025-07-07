7689bd3ad01e60ef139726545d5f1a58f4e5a38a
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-06 23:24:26 -0400

    feat: add ActionAsset class with dividend yield calculation
    Se ha añadido una nueva clase `ActionAsset` que hereda de `Asset`. Esta clase incluye una propiedad `DividendYield` para representar el rendimiento de dividendos y valida los parámetros en su constructor. También se ha implementado un método `Calculate` para calcular el valor de los dividendos basado en el rendimiento y el valor del activo.

---
+++ Domain/Entities/Assets/ActionAsset.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Abstractions;
+using Domain.Exceptions;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Assets
+{
+    public class ActionAsset : Asset
+    {
+        public decimal DividendYield { get; set; } // Percentage of dividend yield
+        public ActionAsset(string name, Money value, DateTime dateAcquired, decimal dividendYield) : base(name, value, dateAcquired)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null or empty.", nameof(name));
+            }
+            if (value.Amount < 0)
+            {
+                throw new MoneyNotValidException(value);
+            }
+            if (dateAcquired > DateTime.Now)
+            {
+                throw new DateTimeNotValidException(dateAcquired);
+            }
+            if (dividendYield < 0)
+            {
+                throw new ArgumentException("Dividend yield cannot be negative.", nameof(dividendYield));
+            }
+
+            DividendYield = dividendYield;
+        }
+
+        public override Money Calculate()
+        {
+            return new Money(Value.Amount * DividendYield / 100, Value.Currency);
+        }
+    }
+}
