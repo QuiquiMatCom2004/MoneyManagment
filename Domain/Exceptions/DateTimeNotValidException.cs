3da4084b16a5a809dae9844e835d938744858927
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-06 23:08:34 -0400

    feat: Implemented Date Time Exception

---
+++ Domain/Exceptions/DateTimeNotValidException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class DateTimeNotValidException : Exception
+    {
+        public DateTimeNotValidException() : base("the proporcionate date its not valid in this context") { }
+        public DateTimeNotValidException(DateTime date)
+            : base($"the proporcionate date {date} its not valid in this context") { }
+        public DateTimeNotValidException(DateTime date, DateTime start, DateTime end)
+            : base($"the proporcionate date {date} is not valid in the range between {start} and {end}")
+        {
+        }
+    }
+}
