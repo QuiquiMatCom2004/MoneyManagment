e9a243d68939ccf597f103570fd0e2f67d363df4
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-06 22:25:38 -0400

    feat: Implemented Cash Flow Abstraction

---
+++ Domain/Abstractions/CashFlow.cs
@@ -0,0 +1,29 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Exceptions;
+
+namespace Domain.Abstractions
+{
+    public abstract class CashFlow : Entity, IMoneytisable
+    {
+        public DateTime StartDate { get; protected set; } = DateTime.UtcNow;
+        public DateTime EndDate { get; protected set; } = DateTime.UtcNow;
+        public Money TotalIncome { get; protected set; } = new Money(0, "USD");
+        public Money TotalExpenses { get; protected set; } = new Money(0, "USD");
+        public Money NetCashFlow => TotalIncome - TotalExpenses;
+        protected List<Transaction> Transactions { get; set; } = new List<Transaction>();
+        public virtual void AddTransaction(Transaction transaction)
+        {
+            if(transaction.TransactionDate > EndDate || transaction.TransactionDate < StartDate)
+                throw new TransactionNotValidException(transaction.TransactionDate);
+            if (transaction.Validate())
+                throw new TransactionNotValidException(transaction.Amount);
+            Transactions.Add(transaction);
+        }
+        public abstract Money Calculate();
+    }
+}
