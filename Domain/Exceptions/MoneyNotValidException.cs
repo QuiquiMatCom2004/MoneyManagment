eca0716bb841ca4b3c4d7c95f19ff90e66c4ab5f
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-06 23:09:05 -0400

    feat: Implemented Money Exception

---
+++ Domain/Exceptions/MoneyNotValidException.cs
@@ -0,0 +1,23 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class MoneyNotValidException : Exception
+    {
+        public MoneyNotValidException() : base("The money value is not valid.")
+        {
+        }
+        public MoneyNotValidException(string message) : base(message)
+        {
+        }
+        public MoneyNotValidException(Money money) 
+            : base($"The money value '{money.Amount}' with currency '{money.Currency}' is not valid.")
+        {
+        }
+    }
+}
