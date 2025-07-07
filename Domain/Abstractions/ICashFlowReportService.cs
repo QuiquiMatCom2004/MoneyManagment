01c2312300de9b48c74124d851aee46e677a73aa
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-07 01:20:22 -0400

    feat: add ICashFlowReportService interface
    Se ha añadido la interfaz `ICashFlowReportService` en el archivo `ICashFlowReportService.cs`. Esta interfaz incluye métodos asíncronos para obtener informes de flujo de caja mensuales, informes para un rango de fechas, el flujo de caja neto promedio y los meses con el mayor flujo neto. También se han añadido las directivas `using` necesarias.

---
+++ Domain/Abstractions/ICashFlowReportService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public interface ICashFlowReportService
+    {
+        Task<CashFlow> GetMonthlyReportAsync(int year, int month);
+        Task<IEnumerable<CashFlow>> GetReportForRangeAsync(DateTime startDate, DateTime endDate);
+        Task<Money> GetAverageNetCashFlowAsync(int year);
+        Task<IEnumerable<CashFlow>> GetTopNetMonthsAsync(int count);
+    }
+}
