374f427c1082a56a74d71875a4858cd3acd84259
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-06 22:23:15 -0400

    feat: Implemented interface for process

---
+++ Domain/Abstractions/IProcessable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public interface IProcessable
+    {
+        /// <summary>
+        /// Processes the current instance.
+        /// </summary>
+        /// <returns>A boolean indicating whether the processing was successful.</returns>
+        bool Process();
+    }
+}
