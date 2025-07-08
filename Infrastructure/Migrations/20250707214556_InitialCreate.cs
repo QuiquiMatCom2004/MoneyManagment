baddd52d03dece53161fab5bfeff433353c1112c
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:44:29 -0400

    feat: add Asset table migration and model definition
    Se han añadido nuevas migraciones para crear la tabla `Asset` y definir su modelo en el contexto de la base de datos. La migración `InitialCreate` establece la estructura de la tabla con propiedades clave y se ha implementado un objeto de valor `Value` para manejar `Amount` y `Currency`. También se ha creado la migración `LiabilitysAndAssetsCreateds`, aunque no contiene cambios en sus métodos. Además, se ha actualizado el `ModelSnapshot` para reflejar la nueva estructura del modelo de datos.

---
+++ Infrastructure/Migrations/20250707214556_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Asset",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ValueAmount = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    ValueCurrency = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DateAcquired = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Category = table.Column<int>(type: "int", nullable: false),
+                    Discriminator = table.Column<string>(type: "varchar(13)", maxLength: 13, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DividendYield = table.Column<decimal>(type: "decimal(65,30)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asset", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asset");
+        }
+    }
+}
