190994d304847b9384bf424c3bf5862042ea49b1
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:48:53 -0400

    feat: add CriptoAssetConfiguration for MarketCap
    Se han añadido nuevas directivas `using` para incluir los espacios de nombres necesarios.
    Se ha creado la clase `CriptoAssetConfiguration` que implementa `IEntityTypeConfiguration<CriptoAsset>`,
    configurando la propiedad `MarketCap` de la entidad `CriptoAsset` como obligatoria.

---
+++ Infrastructure/Configurations/CriptoAssetConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Assets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class CriptoAssetConfiguration : IEntityTypeConfiguration<Domain.Entities.Assets.CriptoAsset>
+    {
+        public void Configure(EntityTypeBuilder<CriptoAsset> builder)
+        {
+            builder.Property(c => c.MarketCap)
+                   .IsRequired();
+        }
+    }
+}
