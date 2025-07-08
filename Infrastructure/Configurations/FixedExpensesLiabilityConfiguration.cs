9113ea6138d3724e0f608a7093b4b0211f055666
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 12:07:54 -0400

    feat: add FixedExpensesLiabilityConfiguration class
    Se ha añadido una nueva clase de configuración `FixedExpensesLiabilityConfiguration` que implementa `IEntityTypeConfiguration` para la entidad `FixedExpensesLiability`. Se configura la propiedad `Beneficts`, estableciendo requisitos para `Amount` y `Currency`, y se han añadido las directivas `using` necesarias.

---
+++ Infrastructure/Configurations/FixedExpensesLiabilityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities.Liabilitys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class FixedExpensesLiabilityConfiguration : IEntityTypeConfiguration<Domain.Entities.Liabilitys.FixedExpensesLiability>
+    {
+        public void Configure(EntityTypeBuilder<FixedExpensesLiability> builder)
+        {
+            builder.OwnsOne(e => e.Beneficts, benefictsBuilder =>
+            {
+                benefictsBuilder.Property(m => m.Amount)
+                    .HasColumnName("BenefictAmount")
+                    .IsRequired();
+
+                benefictsBuilder.Property(m => m.Currency)
+                    .HasColumnName("BenefictCurrency")
+                    .HasMaxLength(3)
+                    .IsRequired();
+            });
+        }
+    }
+}
