0aef522d5232aa6e426c2d66c9f0ce55f65c8d94
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:29:11 -0400

    feat: add IRepository<TEntity> interface for CRUD operations
    Se ha añadido una nueva interfaz `IRepository<TEntity>` que define métodos asíncronos para realizar operaciones CRUD sobre entidades genéricas. Los métodos incluyen obtener por ID, obtener todas las entidades, agregar, actualizar y eliminar entidades. También se han importado los espacios de nombres necesarios.

---
+++ Domain/Abstractions/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Abstractions
+{
+    public interface IRepository<TEntity>
+    {
+        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid id);
+    }
+}
