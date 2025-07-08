3f942ce7a396aad3ca4f753468834e419a8f8cf1
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:41:20 -0400

    feat: add LiabilityCreateEndpoint for creating liabilities
    Se han añadido nuevas importaciones y se ha definido la clase `LiabilityCreateEndpoint`. Este endpoint permite solicitudes POST a la ruta "/liabilities" y maneja la creación de responsabilidades, enviando una respuesta con el estado 201 (Creado) al cliente.

---
+++ MoneyManagment/Endpoints/Liability/LiabilityCreateEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Create.Liability;
+using Application.Dtos;
+using FastEndpoints;
+
+namespace MoneyManagment.Endpoints.Liability
+{
+    public class LiabilityCreateEndpoint : Endpoint<LiabilityCommand,LiabilityDto>
+    {
+        public override void Configure()
+        {
+            Post("/liabilities");
+            AllowAnonymous();
+        }
+        public override async Task HandleAsync(LiabilityCommand req, CancellationToken ct)
+        {
+            var response = await req.ExecuteAsync(ct);
+            await SendAsync(response, StatusCodes.Status201Created, cancellation: ct);
+        }
+    }
+}
