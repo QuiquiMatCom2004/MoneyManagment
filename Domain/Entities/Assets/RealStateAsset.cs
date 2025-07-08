11521e438f3b629a56986abde3e67186c0296e6c
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:34:00 -0400

    feat: add RealStateAsset class for real estate management
    Se ha añadido la clase `RealStateAsset` en el archivo `RealStateAsset.cs`, que representa un activo inmobiliario. Esta clase incluye propiedades para el precio del alquiler (`RentPrice`) y los gastos de mantenimiento (`ChoreExpenses`). También se ha implementado un constructor que inicializa estas propiedades y establece la categoría del activo como `RealEstate`. Además, se ha añadido un método `Calculate` que calcula el beneficio o pérdida mensual restando los gastos de los ingresos por alquiler, y lanza una excepción si las divisas de los precios no coinciden.

---
+++ Domain/Entities/Assets/RealStateAsset.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+using Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Assets
+{
+    public class RealStateAsset : Asset
+    {
+        public Money RentPrice { get; set; } // Monthly rent price of the real estate asset
+        public Money ChoreExpenses { get; set; } // Monthly expenses for maintenance and chores 
+
+        public RealStateAsset(string name, Money value, DateTime dateAcquired, Money rentPrice, Money choreExpenses) 
+            : base(name, value, dateAcquired)
+        {
+            RentPrice = rentPrice;
+            ChoreExpenses = choreExpenses;
+            Category = AssetCategorys.RealEstate;
+        }
+
+        public override Money Calculate()
+        {
+            if(RentPrice.Currency != ChoreExpenses.Currency)
+            {
+                throw new CurrencyNotValidOperationException(RentPrice.Currency, ChoreExpenses.Currency);
+            }
+            return RentPrice - ChoreExpenses; // Calculate monthly profit or loss by subtracting chore expenses from rent price
+        }
+    }
+}
