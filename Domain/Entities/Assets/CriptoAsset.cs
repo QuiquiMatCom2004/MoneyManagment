20364e1db75418030b979b518f7952b1d3734842
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:33:19 -0400

    feat: add CriptoAsset class for cryptocurrency handling
    Se ha añadido la clase `CriptoAsset` en `Domain.Entities.Assets`, que hereda de `Asset`. Esta clase incluye propiedades y métodos para gestionar la capitalización de mercado, valida que no sea negativa y establece la categoría como `CriptoCurrency`. También se implementa el método `Calculate` para calcular el valor del activo basado en su capitalización de mercado.

---
+++ Domain/Entities/Assets/CriptoAsset.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Assets
+{
+    public class CriptoAsset : Asset
+    {
+        public decimal MarketCap { get; set; } // Market capitalization of the cryptocurrency
+
+        public CriptoAsset(string name, Money value, DateTime dateAcquired, decimal marketCap) 
+            : base(name, value, dateAcquired)
+        {
+            if (marketCap < 0)
+            {
+                throw new ArgumentException("Market cap cannot be negative.", nameof(marketCap));
+            }
+            MarketCap = marketCap;
+            Category = AssetCategorys.CriptoCurrency;
+        }
+        public override Money Calculate()
+        {
+            return new Money(Value.Amount * MarketCap / 100, Value.Currency);
+        }
+    }
+}
