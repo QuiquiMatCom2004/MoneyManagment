f7cb9373218458b9051a75cd78e7ceddf9dbfffc
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:38:23 -0400

    feat: add LiabilityMapper with ToLiabilityDto method
    Se han añadido nuevas líneas de código al archivo `LiabilityMapper.cs`.
    Se ha importado el espacio de nombres `System` y otros relacionados.
    Se ha definido una nueva clase estática `LiabilityMapper` que incluye un método de extensión `ToLiabilityDto` para convertir objetos `Liability` en `LiabilityDto`, asignando las propiedades relevantes.

---
+++ Application/Mappers/LiabilityMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public static class LiabilityMapper
+    {
+        public static LiabilityDto ToLiabilityDto(this Domain.Abstractions.Liability liability)
+        {
+            return new LiabilityDto
+            {
+                id = liability.Id,
+                Name = liability.Name,
+                Value = liability.Value,
+                DateIncurred = liability.DateIncurred,
+                MonthlyExpense = liability.MonthlyExpense,
+                Category = liability.Categorys.LiabilityToString()
+            };
+        }
+    }
+}
