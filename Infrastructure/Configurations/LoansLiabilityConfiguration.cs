f73bebada89d306175457d485d59a042195bad51
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 12:08:08 -0400

    feat: add LoansLiability configuration
    Added using directives for Entity Framework and implemented
    IEntityTypeConfiguration in LoansLiabilityConfiguration.
    Configured InterestRate and MonthsToPay properties with
    column names and required constraints.

---
+++ Infrastructure/Configurations/LoansLiabilityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.Liabilitys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class LoansLiabilityConfiguration : IEntityTypeConfiguration<Domain.Entities.Liabilitys.LoansLiability>
+    {
+        public void Configure(EntityTypeBuilder<LoansLiability> builder)
+        {
+            builder.Property(e => e.InterestRate)
+                .HasColumnName("InterestRate")
+                .IsRequired();
+            builder.Property(e => e.MonthsToPay)
+                .HasColumnName("Month to Pay")
+                .IsRequired();
+        }
+    }
+}
