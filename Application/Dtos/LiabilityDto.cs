7270710404881b7eca9e3b1dac55be972794b7bb
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:36:49 -0400

    feat: add LiabilityDto class with default properties
    Se ha añadido la clase `LiabilityDto` en el archivo `LiabilityDto.cs`, que hereda de `EntityDto`. Esta clase contiene propiedades para `Name`, `Value`, `DateIncurred`, `MonthlyExpense` y `Category`, cada una con valores predeterminados. También se han agregado las directivas `using` necesarias para las dependencias de la clase, incluyendo `Domain.ValueObjects`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text` y `System.Threading.Tasks`.

---
+++ Application/Dtos/LiabilityDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class LiabilityDto :EntityDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public Money Value { get; set; } = new Money(0, "USD");
+        public DateTime DateIncurred { get; set; } = DateTime.Now;
+        public Money MonthlyExpense { get; set; } = new Money(0, "USD");
+        public string Category { get; set; } = string.Empty;
+    }
+}
