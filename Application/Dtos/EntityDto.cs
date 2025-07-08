983d1b483839d861a53e6cfba2d3e14cc07f9edc
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:37:49 -0400

    feat: add EntityDto class with id property
    Se han añadido varias directivas `using` al archivo `EntityDto.cs` y se ha creado una nueva clase pública `EntityDto` en el espacio de nombres `Application.Dtos`. Esta clase incluye una propiedad `id` de tipo `Guid`, inicializada con `Guid.Empty`.

---
+++ Application/Dtos/EntityDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class EntityDto
+    {
+        public Guid id { get;set; } = Guid.Empty;  
+    }
+}
