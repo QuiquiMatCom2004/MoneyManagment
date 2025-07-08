864e1641ea10087a9679db4b139f96724a39dd72
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:44:06 -0400

    feat: add ActionAssetConfiguration class
    Added a new class `ActionAssetConfiguration` to configure the `ActionAsset` entity. The `DividendYield` property is now required. Included necessary using directives for proper functionality.

---
+++ Infrastructure/Configurations/ActionAssetConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities.Assets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class ActionAssetConfiguration : IEntityTypeConfiguration<ActionAsset>
+    {
+        public void Configure(EntityTypeBuilder<ActionAsset> builder)
+        {
+            builder.Property(a => a.DividendYield)
+                   .IsRequired();
+        }
+    }
+}
