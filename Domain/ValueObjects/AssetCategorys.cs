56543475267cf3cc0711fddd2bbf1da0d993faca
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:32:28 -0400

    feat: add asset and liability categories enums
    Se han añadido dos enumeraciones: `AssetCategorys` y `LiabilityCategorys`, que representan diferentes categorías de activos y pasivos, respectivamente. También se ha creado la clase estática `CategorysExtensions` con métodos de extensión para convertir cadenas a estas enumeraciones y viceversa, utilizando expresiones switch para el mapeo y manejo de casos no válidos.

---
+++ Domain/ValueObjects/AssetCategorys.cs
@@ -0,0 +1,63 @@
+﻿public enum AssetCategorys
+{
+    RawMaterial,
+    RealEstate,
+    Investment,
+    CriptoCurrency,
+    Actions,
+    Other
+}
+public enum LiabilityCategorys
+{
+    CreditCard,
+    Loan,
+    FixedExpenses,
+    Other
+}
+public static class CategorysExtensions
+{
+    public static AssetCategorys ToAssetCategory(this string category)
+    {
+        return category switch
+        {
+            "RawMaterials" => AssetCategorys.RawMaterial,
+            "Actions" => AssetCategorys.Actions,
+            "Cryptocurrencies" => AssetCategorys.CriptoCurrency,
+            "Investments" => AssetCategorys.Investment,
+            "RealEstate" => AssetCategorys.RealEstate,
+            _ => throw new ArgumentException("Invalid category")
+        };
+    }
+    public static string AssetToString(this AssetCategorys category)
+    {
+        return category switch
+        {
+            AssetCategorys.RawMaterial => "RawMaterials",
+            AssetCategorys.Actions => "Actions",
+            AssetCategorys.CriptoCurrency => "Cryptocurrencies",
+            AssetCategorys.Investment => "Investments",
+            AssetCategorys.RealEstate => "RealEstate",
+            _ => "Other"
+        };
+    }
+    public static LiabilityCategorys ToLiabilityCategory(this string category)
+    {
+        return category switch
+        {
+            "CreditCard" => LiabilityCategorys.CreditCard,
+            "Loan" => LiabilityCategorys.Loan,
+            "FixedExpenses" => LiabilityCategorys.FixedExpenses,
+            _ => throw new ArgumentException("Invalid category")
+        };
+    }
+    public static string LiabilityToString(this LiabilityCategorys category)
+    {
+        return category switch
+        {
+            LiabilityCategorys.CreditCard => "CreditCard",
+            LiabilityCategorys.Loan => "Loan",
+            LiabilityCategorys.FixedExpenses => "FixedExpenses",
+            _ => "Other"
+        };
+    }
+}
