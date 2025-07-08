7331dc47238a08864bd8851d0b9e34fa4936f30a
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:33:43 -0400

    feat: add RawMaterialsAsset class with pricing logic
    Se ha añadido la clase `RawMaterialsAsset` que hereda de `Asset`. Esta clase gestiona el precio de venta de los materiales en bruto y calcula la ganancia o pérdida, lanzando una excepción si las divisas no coinciden.

---
+++ Domain/Entities/Assets/RawMaterialsAsset.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Abstractions;
+using Domain.Exceptions;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Assets
+{
+    public class RawMaterialsAsset : Asset
+    {
+
+        public Money SellPrice { get; set; } // Price at which the raw material can be sold
+        public RawMaterialsAsset(string name, Money value, DateTime dateAcquired, Money sell) : base(name, value, dateAcquired)
+        {
+            SellPrice = sell;
+            Category = AssetCategorys.RawMaterial;
+        }
+        
+        public override Money Calculate()
+        {
+            if(SellPrice.Currency != Value.Currency)
+            {
+                throw new CurrencyNotValidOperationException(SellPrice.Currency, Value.Currency);
+            }
+            return SellPrice - Value; // Calculate profit or loss by subtracting the value from the sell price
+        }
+    }
+}
