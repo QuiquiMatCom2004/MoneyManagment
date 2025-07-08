d1eb48467630cda3c4a24b84c10f9de465ebb2cb
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:41:06 -0400

    feat: add AssetCreateEndpoint for asset creation
    Se ha añadido una nueva clase `AssetCreateEndpoint` en el espacio de nombres `MoneyManagment.Endpoints.Asset`. Esta clase permite manejar la creación de activos mediante la definición de una ruta y el manejo de solicitudes anónimas. Se implementaron los métodos `Configure` y `HandleAsync` para gestionar la creación de activos y enviar respuestas adecuadas.

---
+++ MoneyManagment/Endpoints/Asset/AssetCreateEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Create.Assets;
+using FastEndpoints;
+
+namespace MoneyManagment.Endpoints.Asset
+{
+    public class AssetCreateEndpoint : Endpoint<Application.Features.Create.Assets.AssetCommand, Application.Dtos.AssetDto>
+    {
+        public override void Configure()
+        {
+            Post("/assets");
+            AllowAnonymous();
+        }
+        public override async Task HandleAsync(AssetCommand req, CancellationToken ct)
+        {
+            var response = await req.ExecuteAsync(ct);
+            await SendAsync(response, StatusCodes.Status201Created,cancellation: ct);
+        }
+    }
+}
