019f4912a9986554bc788cda05423de8c8de523e
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 12:07:13 -0400

    feat: add RawMaterialAssetConfiguration class
    Added several using directives and implemented the
    RawMaterialAssetConfiguration class to configure
    SellPrice properties for RawMaterialsAsset, ensuring
    Amount and Currency are required with specific column names.

---
+++ Infrastructure/Configurations/RawMaterialAssetConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class RawMaterialAssetConfiguration : IEntityTypeConfiguration<Domain.Entities.Assets.RawMaterialsAsset>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Domain.Entities.Assets.RawMaterialsAsset> builder)
+        {
+            builder.OwnsOne(e => e.SellPrice, price =>
+            {
+                price.Property(p => p.Amount)
+                     .HasColumnName("PriceAmount")
+                     .IsRequired();
+                price.Property(p => p.Currency)
+                     .HasColumnName("PriceCurrency")
+                     .IsRequired();
+            });
+        }
+    }
+}
