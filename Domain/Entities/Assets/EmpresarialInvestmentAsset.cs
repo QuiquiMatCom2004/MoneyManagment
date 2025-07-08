6ce8b8bc87a04118e4c4da73603a174d93194329
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:33:33 -0400

    feat: add EmpresarialInvestmentAsset class
    Se ha añadido una nueva clase `EmpresarialInvestmentAsset` en el espacio de nombres `Domain.Entities.Assets`. Esta clase hereda de `Asset` y tiene propiedades para el porcentaje de inversión (`PorcentageOfInvestment`) y el rendimiento anual (`AnualPerformace`). Se incluye un constructor que valida que el porcentaje de inversión esté entre 0 y 100. Además, se ha implementado un método `Calculate` que calcula el rendimiento mensual basado en el rendimiento anual y el porcentaje de inversión.

---
+++ Domain/Entities/Assets/EmpresarialInvestmentAsset.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Assets
+{
+    public class EmpresarialInvestmentAsset : Asset
+    {
+        public decimal PorcentageOfInvestment { get; set; } // Percentage of the investment in the business
+        public Money AnualPerformace { get; set; } // Annual performance of the investment
+        public EmpresarialInvestmentAsset(string name, Money value, DateTime dateAcquired, decimal porcentageOfInvestment, Money anualPerformace) 
+            : base(name, value, dateAcquired)
+        {
+            if (porcentageOfInvestment < 0 || porcentageOfInvestment > 100)
+            {
+                throw new ArgumentException("Percentage of investment must be between 0 and 100.", nameof(porcentageOfInvestment));
+            }
+            PorcentageOfInvestment = porcentageOfInvestment;
+            AnualPerformace = anualPerformace;
+            Category = AssetCategorys.Investment;
+        }
+        public override Money Calculate()
+        {
+            return new Money((AnualPerformace.Amount * PorcentageOfInvestment / 100) /12, AnualPerformace.Currency);
+        }
+    }
+}
