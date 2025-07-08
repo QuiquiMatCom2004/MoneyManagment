3eda0cf4e370e717d02d45de0ecd2f3c5be237a9
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:42:24 -0400

    feat: add dependency registration for asset and liability services
    Added necessary `using` directives and created a static class `Dependecies` with an extension method `RegisterServices` to register services in the dependency injection container. Registered `AssetCommandHandler`, `AssetService`, `LiabilityService`, and `UnitOfWork`. Commented out lines for potential future registration of `LiabilityCommandHandler` and a generic repository.

---
+++ MoneyManagment/Dependecies.cs
@@ -0,0 +1,33 @@
+﻿
+using Application.Abstractions.IAssetService;
+using Application.Abstractions.ILiabilityService;
+using Application.Dtos;
+using Application.Features.Create.Assets;
+using Application.Features.Create.Liability;
+using Application.Services.Assets;
+using Application.Services.Liabilitys;
+using Domain.Abstractions;
+using FastEndpoints;
+using Infrastructure.DB;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace MoneyManagment
+{
+    public static class Dependecies
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            // Register your services here
+            services.AddScoped<CommandHandler<AssetCommand,AssetDto>,AssetCommandHandler>();
+            //services.AddScoped<LiabilityCommandHandler>();
+            
+            services.AddScoped<IAssetServiceCreate, AssetService>();
+            services.AddScoped<ILiabilityServiceCreate, LiabilityService>();
+            
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            //services.TryAddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+            return services;
+        }
+    }
+}
