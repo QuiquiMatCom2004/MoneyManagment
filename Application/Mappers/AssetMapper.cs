ff135427cfd61dc0aa714ed0fda80eafe62c0fa7
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:38:37 -0400

    feat: add AssetMapper and ToAssetDto method
    Se han añadido nuevas importaciones al archivo `AssetMapper.cs` y se ha implementado la clase estática `AssetMapper`. Esta clase incluye un método de extensión `ToAssetDto` que convierte un objeto `Asset` en un objeto `AssetDto`, asignando las propiedades relevantes del objeto original al nuevo objeto creado.

---
+++ Application/Mappers/AssetMapper.cs
@@ -0,0 +1,26 @@
+﻿using Application.Dtos;
+using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers
+{
+    public static class AssetMapper
+    {
+        public static AssetDto ToAssetDto(this Asset asset)
+        {
+            return new AssetDto
+            {
+                id = asset.Id,
+                Name = asset.Name,
+                Value = asset.Value,
+                DateAcquired = asset.DateAcquired,
+                MonthlyIncome = asset.MonthlyIncome,
+                Category = asset.Category.AssetToString()
+            };
+        }
+    }
+}
