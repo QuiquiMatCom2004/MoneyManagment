188aaf7c8550c7c96a489ee67f9d012446d97f71
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:35:06 -0400

    feat: add IAssetServiceCreate interface for asset creation
    Se ha añadido un nuevo archivo de interfaz `IAssetServiceCreate` en el espacio de nombres `Application.Abstractions`. Esta interfaz define métodos para crear diferentes tipos de activos, incluyendo acciones, criptomonedas, inversiones empresariales, bienes raíces y materias primas. Se han importado los espacios de nombres necesarios para su funcionamiento, como `Application.Dtos`, `Application.Features.Create.Assets` y `Domain.Entities.Assets`.

---
+++ Application/Abstractions/IAssetService/IAssetServiceCreate.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos;
+using Application.Features.Create.Assets;
+using Domain.Entities.Assets;
+
+namespace Application.Abstractions.IAssetService
+{
+    public interface IAssetServiceCreate
+    {
+        ActionAsset CreateActions(AssetCommand command);
+        CriptoAsset CreateCryptocurrency(AssetCommand command);
+        EmpresarialInvestmentAsset CreateEmpresarialInvestment(AssetCommand command);
+        RealStateAsset CreateRealEstate(AssetCommand command);
+        RawMaterialsAsset CreateRawMaterials(AssetCommand command);
+    }
+}
