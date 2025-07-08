edf6861679f8223e077e94c36daacd72ca1f59ae
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 12:07:42 -0400

    feat: add CreditCardLiability configuration
    Added necessary using directives for entity namespaces and Entity Framework Core. Implemented the CreditCardLiabilityConfiguration class to configure the CreditCardLiability entity, defining required properties InterestRate and MinimumPaymentPercentage with their corresponding column names in the database.

---
+++ Infrastructure/Configurations/CreditCardLiabilityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities.Liabilitys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class CreditCardLiabilityConfiguration : IEntityTypeConfiguration<Domain.Entities.Liabilitys.CreditCardLiability>
+    {
+        public void Configure(EntityTypeBuilder<CreditCardLiability> builder)
+        {
+            builder.Property(e => e.InterestRate)
+                .HasColumnName("InterestRate")
+                .IsRequired();
+            builder.Property(e => e.MinimumPaymentPercentage)
+                .HasColumnName("MinimumPaymentPercentage")
+                .IsRequired();
+        }
+    }
+}
