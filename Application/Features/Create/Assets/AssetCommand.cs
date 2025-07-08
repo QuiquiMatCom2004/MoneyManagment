ee575cc7847c65b07fb018724c1351b33c40a862
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:36:05 -0400

    feat: add AssetCommand and AssetCommandHandler
    Se ha añadido la clase `AssetCommand` para representar un comando de creación de activos, incluyendo propiedades relevantes para diferentes tipos de activos. Además, se ha implementado `AssetCommandHandler` para gestionar la ejecución del comando y la creación de activos según su categoría, utilizando un servicio de creación y un patrón de unidad de trabajo.

---
+++ Application/Features/Create/Assets/AssetCommand.cs
@@ -0,0 +1,25 @@
+﻿using Application.Dtos;
+
+namespace Application.Features.Create.Assets
+{
+    public record AssetCommand : ICommand<AssetDto>
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Value { get; set; } = 0;
+        public string Currency { get; set; } = string.Empty;
+        public DateTime DateAcquired { get; set; } = DateTime.Now;
+        public string Category { get; set; } = string.Empty;
+        //RawMaterials
+        public decimal? SellPrice { get; set; } = null;
+        //Actions
+        public decimal? DividendYield {  get; set; } = null;
+        //Cryptocurrencies
+        public decimal? MarketCap { get; set; } = null;
+        //Investments
+        public decimal? PorcentageInvestment { get; set; } = null;
+        public decimal? AnualPerformace { get; set; } = null;
+        //RealEstate
+        public decimal? RentPrice { get; set; } = null;
+        public decimal? ChoreExpenses { get; set; } = null;
+    }
+}
---
+++ Application/Features/Create/Assets/AssetCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using Application.Dtos;
+using Application.Mappers;
+using Domain.Abstractions;
+using Domain.Entities.Assets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Create.Assets
+{
+    public class AssetCommandHandler : CommandHandler<AssetCommand, AssetDto>
+    {
+        private readonly IAssetServiceCreate _service;
+        private readonly IUnitOfWork _work;
+        public AssetCommandHandler(IAssetServiceCreate service , IUnitOfWork work)
+        {
+            _service = service;
+            _work = work;
+        }
+        public override async Task<AssetDto> ExecuteAsync(AssetCommand command, CancellationToken ct)
+        {
+            Asset? asset = null;
+            switch (command.Category.ToAssetCategory())
+            {
+                
+                case AssetCategorys.RawMaterial:
+                    asset = _service.CreateRawMaterials(command);
+                    var repo1 = _work.GetRepository<RawMaterialsAsset>();
+                    await repo1.AddAsync(asset as RawMaterialsAsset);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(asset.ToAssetDto());
+                case AssetCategorys.Actions:
+                    asset= _service.CreateActions(command);
+                    var repo2 = _work.GetRepository<ActionAsset>();
+                    await repo2.AddAsync(asset as ActionAsset);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(asset.ToAssetDto());
+                case AssetCategorys.CriptoCurrency:
+                    asset = _service.CreateCryptocurrency(command);
+                    var repo3 = _work.GetRepository<CriptoAsset>();
+                    await repo3.AddAsync(asset as CriptoAsset);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(asset.ToAssetDto());
+                case AssetCategorys.Investment:
+                    asset = _service.CreateEmpresarialInvestment(command);
+                    var repo4 = _work.GetRepository<EmpresarialInvestmentAsset>();
+                    await repo4.AddAsync(asset as EmpresarialInvestmentAsset);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(asset.ToAssetDto());
+                case AssetCategorys.RealEstate:
+                    asset = _service.CreateRealEstate(command);
+                    var repo5 = _work.GetRepository<RealStateAsset>();
+                    await repo5.AddAsync(asset as RealStateAsset);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(asset.ToAssetDto());
+                default:
+                    throw new ArgumentException("Invalid category");
+            }
+
+        }
+    }
+}
