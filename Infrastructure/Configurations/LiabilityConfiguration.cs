1962b8facebc114cf32fa0edff8ed3317c342d78
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 12:06:59 -0400

    feat: add LiabilityConfiguration for entity mapping
    Added a new class `LiabilityConfiguration` that implements
    `IEntityTypeConfiguration<Liability>`. This class configures
    the `Liability` entity using the TPH mapping strategy,
    defines its primary key, and sets up the complex property
    `Value` with required fields `Amount` and `Currency`.

---
+++ Infrastructure/Configurations/LiabilityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class LiabilityConfiguration : IEntityTypeConfiguration<Domain.Abstractions.Liability>
+    {
+        public void Configure(EntityTypeBuilder<Liability> builder)
+        {
+            builder.UseTphMappingStrategy();
+
+            builder.HasKey(l => l.Id);
+            builder.OwnsOne(l => l.Value,
+                money =>
+                {
+                    money.Property(m => m.Amount)
+                        .HasColumnName("ValueAmount")
+                        .IsRequired();
+                    money.Property(m => m.Currency)
+                        .HasColumnName("ValueCurrency")
+                        .IsRequired();
+                }
+            );
+        }
+    }
+}
