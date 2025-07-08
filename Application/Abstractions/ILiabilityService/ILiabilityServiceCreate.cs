c90c9fdcb4c9ff234e5efb672b52aa4372670d86
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:35:18 -0400

    feat: add ILiabilityServiceCreate interface
    Se han añadido nuevas declaraciones de uso y se ha definido una nueva interfaz llamada `ILiabilityServiceCreate` en el archivo `ILiabilityServiceCreate.cs`. Esta interfaz incluye tres métodos: `CreditCardLiability`, `LoanLiability` y `FixedExpensesLiability`, todos los cuales aceptan un parámetro de tipo `LiabilityCommand` y devuelven diferentes tipos de responsabilidad financiera.

---
+++ Application/Abstractions/ILiabilityService/ILiabilityServiceCreate.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Create.Liability;
+using Domain.Entities.Liabilitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions.ILiabilityService
+{
+    public interface ILiabilityServiceCreate
+    {
+        CreditCardLiability CreditCardLiability(LiabilityCommand command);
+        LoansLiability LoanLiability(LiabilityCommand command);
+        FixedExpensesLiability FixedExpensesLiability(LiabilityCommand command);
+    }
+}
