9e3ba9c90d61f10c0af6b8004a7ab67b4ad4e529
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:43:38 -0400

    feat: add UnitOfWork class for repository pattern
    Se ha añadido una nueva clase `UnitOfWork` en el espacio de nombres `Infrastructure.DB` que implementa la interfaz `IUnitOfWork`. Esta clase utiliza `MyDBContext` y permite obtener instancias de repositorios genéricos para entidades específicas. También se ha implementado el método `SaveChangesAsync` para guardar cambios de manera asíncrona en la base de datos.

---
+++ Infrastructure/DB/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DB
+{
+    public class UnitOfWork(MyDBContext context) : IUnitOfWork
+    {
+        private readonly MyDBContext _context = context;
+        private readonly Dictionary<Type, object> _repositories = new Dictionary<Type, object>();
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : Entity
+        {
+            var type = typeof(TEntity);
+
+            if(_repositories.ContainsKey(type))
+                return (IRepository<TEntity>)_repositories[type];
+            
+            var implementationType = typeof(GenericRepository<>).MakeGenericType(typeof(TEntity));
+
+            var instance = Activator.CreateInstance(implementationType, _context);
+
+            if (instance == null)
+            {
+                throw new InvalidOperationException($"Could not create instance of {implementationType.FullName}");
+            }
+
+            _repositories[type] = instance;
+
+            return (IRepository<TEntity>)instance;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
