d9224cee1995e4427409cec530a8c61055da2e56
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:34:39 -0400

    feat: add FixedExpensesLiability class
    Se ha añadido la clase `FixedExpensesLiability` que hereda de `Liability`. Esta clase gestiona los beneficios mensuales relacionados con la responsabilidad fija, incluyendo dos constructores y un método `Calculate` para determinar la responsabilidad neta.

---
+++ Domain/Entities/Liabilitys/FixedExpensesLiability.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Abstractions;
+using Domain.ValueObjects;
+
+namespace Domain.Entities.Liabilitys
+{
+    public class FixedExpensesLiability : Liability
+    {
+        public Money Beneficts { get; set; }// Monthly benefits or income from the liability
+        public FixedExpensesLiability(string name, Money value, DateTime dateIncurred, Money beneficts) 
+            : base(name, value, dateIncurred)
+        {
+            Beneficts = beneficts;
+            Categorys = LiabilityCategorys.FixedExpenses;
+        }
+        public FixedExpensesLiability(string name, Money value, DateTime dateIncurred) 
+            : base(name, value, dateIncurred)
+        {
+            Beneficts = new Money(0,value.Currency);
+        }
+        public override Money Calculate()
+        {
+            return Value - Beneficts; // Calculate the net liability by subtracting benefits from the value
+        }
+    }
+}
