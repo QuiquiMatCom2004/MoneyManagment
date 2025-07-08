560b4984976389829f0afa22d3bff9e36d0557ad
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:37:04 -0400

    feat: add LiabilityCommand and handler logic
    Se ha añadido un nuevo archivo `LiabilityCommand.cs` que define un registro `LiabilityCommand` con propiedades relacionadas con las obligaciones. Además, se ha modificado `LiabilityCommandHandler.cs` para implementar la lógica de manejo de comandos para `LiabilityCommand`, gestionando diferentes categorías de obligaciones y utilizando un servicio para crear y almacenar las instancias correspondientes.

---
+++ Application/Features/Create/Liability/LiabilityCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Features.Create.Liability
+{
+    public record LiabilityCommand : ICommand<LiabilityDto>
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Amount { get; set; } = 0;
+        public string Currency { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; } = DateTime.Now;
+        public string Category { get; set; } = string.Empty;
+        public decimal? InterestRate { get; set; } = null;
+        //Credit Cards
+        public decimal? MinimumPaymentPercentage { get; set; } = null;
+        //Loans
+        public int? MonthToPay { get; set; } = null;
+        //FixedExpenses
+        public decimal? Beneficts { get; set; } = null;
+    }
+}
---
+++ Application/Features/Create/Liability/LiabilityCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Application.Mappers;
+using Domain.Abstractions;
+using Domain.Entities.Liabilitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Create.Liability
+{
+    public class LiabilityCommandHandler (ILiabilityServiceCreate service, IUnitOfWork work): ICommandHandler<LiabilityCommand, LiabilityDto>
+    {
+        private readonly ILiabilityServiceCreate _service = service;
+        private readonly IUnitOfWork _work = work;
+        async Task<LiabilityDto> ICommandHandler<LiabilityCommand, LiabilityDto>.ExecuteAsync(LiabilityCommand command, CancellationToken ct)
+        {
+            Domain.Abstractions.Liability liability = null;
+            switch(command.Category.ToLiabilityCategory())
+            {
+                case LiabilityCategorys.CreditCard:
+                    liability = _service.CreditCardLiability(command);
+                    var repo1 = _work.GetRepository<CreditCardLiability>();
+                    await repo1.AddAsync(liability as CreditCardLiability);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(liability.ToLiabilityDto());
+                case LiabilityCategorys.Loan:
+                    liability = _service.LoanLiability(command);
+                    var repo2 = _work.GetRepository<LoansLiability>();
+                    await repo2.AddAsync(liability as LoansLiability);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(liability.ToLiabilityDto());
+                case LiabilityCategorys.FixedExpenses:
+                    liability = _service.FixedExpensesLiability(command);
+                    var repo3 = _work.GetRepository<FixedExpensesLiability>();
+                    await repo3.AddAsync(liability as FixedExpensesLiability);
+                    await _work.SaveChangesAsync();
+                    return await Task.FromResult(liability.ToLiabilityDto());
+                default:
+                    throw new ArgumentException("Invalid liability category", nameof(command.Category));
+            }
+        }
+    }
+}
