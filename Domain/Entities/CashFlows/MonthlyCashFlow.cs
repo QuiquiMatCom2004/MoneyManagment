e21c234a5fefb9203ff595bd0e863ec22ea24195
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:34:16 -0400

    feat: add MonthlyCashFlow class for cash flow calculations
    Se ha añadido la clase `MonthlyCashFlow` en el espacio de nombres `Domain.Entities.CashFlows`. Esta clase hereda de `CashFlow` y tiene un constructor que inicializa `TotalExpenses` y `TotalIncome` como instancias de `Money` con un valor inicial de 0. Además, se ha implementado el método `Calculate`, que calcula el flujo de caja neto sumando los ingresos y restando los gastos a partir de una colección de transacciones, que pueden ser de diferentes tipos (ingresos, gastos, inversiones, amortizaciones y retiros).

---
+++ Domain/Entities/CashFlows/MonthlyCashFlow.cs
@@ -0,0 +1,41 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.CashFlows
+{
+    public class MonthlyCashFlow : CashFlow
+    {
+        public MonthlyCashFlow(string Currency, DateTime StartDate, DateTime EndDate) : base(StartDate,EndDate)
+        {
+            TotalExpenses = new Money(0, Currency);
+            TotalIncome = new Money(0, Currency);
+        }
+        public override Money Calculate()
+        {
+            TotalIncome = new Money(0, TotalIncome.Currency);
+            TotalExpenses = new Money(0,TotalExpenses.Currency);
+            foreach(var transaction in Transactions)
+            {
+                switch (transaction.Type)
+                {
+                    case TransactionType.Income:
+                        TotalIncome += transaction.Amount;
+                        break;
+                    case TransactionType.Expense:
+                    case TransactionType.Investment:
+                    case TransactionType.Amotization:
+                    case TransactionType.Withdrawal:
+                        TotalExpenses += transaction.Amount;
+                        break;
+                    default: continue;
+                }
+            }
+            return NetCashFlow;
+        }
+    }
+}
