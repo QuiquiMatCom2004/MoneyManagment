38dc0d433ac19b65be8b8a18ca1346f58d429684
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:43:17 -0400

    feat: add MyDBContext with EF Core configuration
    Se han añadido varias directivas `using` en `MyDBContext.cs` y se ha implementado la clase `MyDBContext`, que hereda de `DbContext`. Se ha sobrescrito el método `OnModelCreating` para aplicar configuraciones del modelo desde el ensamblado actual.

---
+++ Infrastructure/DB/MyDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DB
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
