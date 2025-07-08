8f78a66725e75a803df8dca9e4246c9df69ffc29
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:51:30 -0400

    feat: add EmpresarialInvestmentAsset configuration
    Se ha añadido la clase `EmpresarialInvestmentAssetConfiguration` que implementa `IEntityTypeConfiguration` para la entidad `EmpresarialInvestmentAsset`. Se definen las propiedades `PorcentageOfInvestment` y `AnualPerformace`, estableciendo requisitos de obligatoriedad y nombres de columna personalizados.

---
+++ Infrastructure/Configurations/EmpresarialInvestmentAssetConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities.Assets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class EmpresarialInvestmentAssetConfiguration : IEntityTypeConfiguration<Domain.Entities.Assets.EmpresarialInvestmentAsset>
+    {
+        public void Configure(EntityTypeBuilder<EmpresarialInvestmentAsset> builder)
+        {
+            builder.Property(e => e.PorcentageOfInvestment)
+                   .IsRequired();
+            builder.OwnsOne(e => e.AnualPerformace, anualPerformace =>
+                {
+                    anualPerformace.Property(a => a.Amount)
+                                   .HasColumnName("AnualPerformaceAmount")
+                                   .IsRequired();
+                    anualPerformace.Property(a => a.Currency)
+                                   .HasColumnName("AnualPerformaceCurrency")
+                                   .IsRequired();
+                });
+        }
+    }
+}
