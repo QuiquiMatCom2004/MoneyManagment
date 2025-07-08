19c39ef4541cc349dad65f28ab26bbde68401dd1
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 12:07:29 -0400

    feat: add RealStateAssetConfiguration class
    Se ha añadido una nueva clase de configuración `RealStateAssetConfiguration` que implementa `IEntityTypeConfiguration` para la entidad `RealStateAsset`. Esta clase configura las propiedades `RentPrice` y `ChoreExpenses`, definiendo los nombres de las columnas y marcándolas como obligatorias.

---
+++ Infrastructure/Configurations/RealStateAssetConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities.Assets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configurations
+{
+    public class RealStateAssetConfiguration : IEntityTypeConfiguration<Domain.Entities.Assets.RealStateAsset>
+    {
+        public void Configure(EntityTypeBuilder<RealStateAsset> builder)
+        {
+            builder.OwnsOne(e => e.RentPrice, rentalIncome =>
+            {
+                rentalIncome.Property(r => r.Amount)
+                             .HasColumnName("RentalIncomeAmount")
+                             .IsRequired();
+                rentalIncome.Property(r => r.Currency)
+                             .HasColumnName("RentalIncomeCurrency")
+                             .IsRequired();
+            });
+            builder.OwnsOne(e => e.ChoreExpenses, tax =>
+            {
+                tax.Property(t => t.Amount)
+                    .HasColumnName("ChoreExpensesAmount")
+                    .IsRequired();
+                tax.Property(t => t.Currency)
+                    .HasColumnName("ChoreExpensesCurrency")
+                    .IsRequired();
+            });
+        }
+    }
+}
