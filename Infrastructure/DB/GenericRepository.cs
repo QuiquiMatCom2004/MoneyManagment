825fa82906a8c398e3c25b2b2d2854897d0057db
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:43:02 -0400

    feat: add GenericRepository for CRUD operations
    Se ha añadido una nueva clase llamada `GenericRepository<TEntity>` que implementa la interfaz `IRepository<TEntity>`. Esta clase proporciona métodos para realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) en una entidad de tipo `TEntity` utilizando Entity Framework Core. Se han incluido métodos para obtener una entidad por su ID, obtener todas las entidades, agregar una nueva entidad, actualizar una entidad existente y eliminar una entidad por su ID. Además, se ha añadido el contexto de la base de datos `MyDBContext` para interactuar con la base de datos.

---
+++ Infrastructure/DB/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DB
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        private readonly MyDBContext _context;
+        public GenericRepository(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<TEntity> Table => _context.Set<TEntity>();
+
+        public async Task<TEntity?> GetByIdAsync(Guid id)
+        {
+            return await Table.FindAsync(id);
+        }
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await Table.ToListAsync();
+        }
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await Table.AddAsync(entity);
+            return entity;
+        }
+
+        public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            Table.Update(entity);
+            return Task.FromResult(entity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                Table.Remove(entity);
+            }
+        }
+    }
+}
