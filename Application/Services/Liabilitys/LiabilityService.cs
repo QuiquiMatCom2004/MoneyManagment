6972fed59a7df51e2928af26b0f6c34a0f697daa
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-07-08 11:37:23 -0400

    feat: add LiabilityService for financial responsibilities
    Se ha añadido la clase `LiabilityService` en el espacio de nombres `Application.Services.Liabilitys`, que implementa la interfaz `ILiabilityServiceCreate`. Esta clase contiene métodos para crear diferentes tipos de responsabilidades financieras: `CreditCardLiability`, `FixedExpensesLiability` y `LoanLiability`. Cada método toma un objeto `LiabilityCommand` como parámetro y devuelve una instancia del tipo correspondiente, utilizando propiedades del comando para inicializar los objetos de responsabilidad.

---
+++ Application/Services/Liabilitys/LiabilityService.cs
@@ -0,0 +1,53 @@
+﻿using Application.Features.Create.Liability;
+using Domain.Entities.Liabilitys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Liabilitys
+{
+    public class LiabilityService : ILiabilityServiceCreate
+    {
+        public CreditCardLiability CreditCardLiability(LiabilityCommand command)
+        {
+            return new CreditCardLiability(
+                command.Name,
+                new Domain.ValueObjects.Money(command.Amount, command.Currency),
+                command.DueDate,
+                command.InterestRate?? 0,
+                command.MinimumPaymentPercentage ?? 0
+                );
+        }
+
+        public FixedExpensesLiability FixedExpensesLiability(LiabilityCommand command)
+        {
+            if(command.Beneficts.HasValue)
+            {
+                return new FixedExpensesLiability(
+                    command.Name,
+                    new Domain.ValueObjects.Money(command.Amount, command.Currency),
+                    command.DueDate,
+                    new Domain.ValueObjects.Money(command.Beneficts.Value, command.Currency)
+                );
+            }
+            return new FixedExpensesLiability(
+                command.Name,
+                new Domain.ValueObjects.Money(command.Amount, command.Currency),
+                command.DueDate
+            );
+        }
+
+        public LoansLiability LoanLiability(LiabilityCommand command)
+        {
+            return new LoansLiability(
+                command.Name,
+                new Domain.ValueObjects.Money(command.Amount, command.Currency),
+                command.DueDate,
+                command.InterestRate ?? 0,
+                command.MonthToPay ?? 0
+            );
+        }
+    }
+}
